3c9966bfbb627a6da069c7d38903fc67407c4691
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-01-31 17:16:53 +0100

    Fix: Corrected GitHub repository Email

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 // Define the API endpoint
 app.get("/", (req, res) => {
   res.json({
-    email: "olatoyesefaruq@example.com",
+    email: "olatoyesefaruq@gmail.com",
     current_datetime: new Date().toISOString(),
     github_url: "https://github.com/Wheezy049/HNG-Api"
   });
